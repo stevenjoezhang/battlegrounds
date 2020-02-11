88e1f152f3c217b744f75a9b9e56a352049c7366
Mimi <1119186082@qq.com>
2020-02-11 14:14:22 +0800

    Remove wrong minions from random pool

--- database.py
+++ database.py
@@ -4,10 +4,10 @@ with codecs.open('data.json', encoding='utf8') as f:
   database = json.load(f)
 
 def get_minions_by_cost(cost):
-  return list(filter(lambda x: x['cost'] == cost, database))
+  return list(filter(lambda x: x['isBaconPoolMinion'] == True and x['cost'] == cost, database))
 
 def get_deathrattle_minions():
-  return list(filter(lambda x: x['deathrattle'] == True, database))
+  return list(filter(lambda x: x['isBaconPoolMinion'] == True and x['deathrattle'] == True, database))
 
 def get_legendary_minions():
-  return list(filter(lambda x: x['legendary'] == True, database))
+  return list(filter(lambda x: x['isBaconPoolMinion'] == True and x['legendary'] == True, database))
